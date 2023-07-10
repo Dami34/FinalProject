39c0bc15690f5e4d1c7d54474c5afa138783105d
Dami <damisi.odumosu03@gmail.com>
2023-07-10 17:45:37 -0500

    Added Volume Slider and New Build

--- Final/Assets/Scripts/ChangeOptions.cs
+++ Final/Assets/Scripts/ChangeOptions.cs
@@ -1,28 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeOptions : MonoBehaviour
 {
     List<int> widths = new List<int>() {568, 960, 1280, 1920}; //widths for resolution
     List<int> heights = new List<int>() {320, 540, 800, 1080}; //heights for resolution
+    [SerializeField] Slider volumeSlider; // Variable for volume
 
-    public void SetScreenSize(int index)
+    public void SetScreenSize(int ind)
     {
         // Check if screen is fullscreen and set it
         bool fullscreen = Screen.fullScreen;
         // Get width
-        int width = widths[index];
+        int width = widths[ind];
         // Get height
-        int height = heights[index];
+        int height = heights[ind];
 
         // Set the resolution
         Screen.SetResolution(width, height, fullscreen);
     }
 
-    public void SetFullScreen(bool _fullscreen)
+    public void SetFullScreen(bool fullS)
     {
         // Check if screen is fullscreen and set it
-        Screen.fullScreen = _fullscreen;
+        Screen.fullScreen = fullS;
+    }
+
+    void Awake()
+    {
+        // Set volume to previous volume if set when game starts
+        if(PlayerPrefs.HasKey("Volume"))
+        {
+            SetVolume(PlayerPrefs.GetFloat("Volume"));
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
+    }
+
+    public void SetVolume(float vol)
+    {
+        // Set volume to the volume chosen
+        AudioListener.volume = vol;
+        // Save volume for next game
+        PlayerPrefs.SetFloat("Volume", vol);
     }
 }
